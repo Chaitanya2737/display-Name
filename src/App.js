36c16a362a24006262f2ca05b49905433b7493fc
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-05-17 19:32:56 +0530

    testcase falling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
   const [name, setName] = useState({
@@ -8,16 +8,18 @@ function App() {
     lname: ""
   });
 
-  const [showdata, setshowdata] = useState(false)
+  const [showData, setShowData] = useState(false);
 
   const handleChange = (e) => {
-    setName({ ...name, [e.target.name]: e.target.value });
+    setName({
+      ...name,
+      [e.target.name]: e.target.value
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle the form submission logic here
-    setshowdata(true)
+    setShowData(true);
   };
 
   return (
@@ -26,21 +28,35 @@ function App() {
         <h1>Full Name Display</h1>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="fname">First Name</label>
-            <input onChange={handleChange} type="text" id="fname" value={name.fname} name="fname" required />
+            <label htmlFor="fname">First Name:</label>
+            <input 
+              onChange={handleChange} 
+              type="text" 
+              id="fname" 
+              value={name.fname} 
+              name="fname" 
+              required 
+            />
           </div>
           <div>
-            <label htmlFor="lname">Last Name</label>
-            <input onChange={handleChange} type="text" id="lname" value={name.lname} name="lname"  required/>
+            <label htmlFor="lname">Last Name:</label>
+            <input 
+              onChange={handleChange} 
+              type="text" 
+              id="lname" 
+              value={name.lname} 
+              name="lname" 
+              required 
+            />
           </div>
           <button type="submit">Submit</button>
         </form>
       </div>
-     {
-      showdata && <div>
-      Full Name : {name.fname} {name.lname}
-    </div>
-     } 
+      {showData && (
+        <div>
+          Full Name: {name.fname} {name.lname}
+        </div>
+      )}
     </>
   );
 }
